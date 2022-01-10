e6299db1a869bfefde7cdbf37f6304d74292b825
Medeiross03 <matheusmedeiros2003@gmail.com>
2022-01-09 23:01:09 -0300

    estilizando o site

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from conta.views import listar_contas,conta,cadastrar_contas,excluir_conta
 from django.views.generic import RedirectView
-
+from .views import PaginaPrincipal
 app_name = "paginas"
 
 urlpatterns = [
@@ -9,5 +9,5 @@ urlpatterns = [
     path('contas/conta/',conta, name="conta"),
     path('contas/conta/submit',cadastrar_contas, name="submit"),
     path('contas/conta/delete/<int:id_conta>',excluir_conta, name="delete"),
-    path('home/',RedirectView.as_view(url="contas/")),
+    path('',PaginaPrincipal.as_view(), name="home"),
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class PaginaPrincipal(TemplateView):
+    template_name = "base/home.html"
